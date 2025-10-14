108eb956b07e8366c153199be3e90ea7165bcd3c
Thomas Assheton <ta21@sanger.ac.uk>
2025-10-14 11:39:17 +0100

    Add User class

---
+++ src/main/java/User.java
@@ -0,0 +1,35 @@
+public class User {
+    private String name;
+    private String emailAddress;
+    private String phoneNumber;
+
+    public User(String name, String emailAddress, String phoneNumber) {
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) throws IllegalArgumentException {
+        this.phoneNumber = phoneNumber;
+    }
+}
