7a3b9b31cd896e0ed6cc52b74a44908f318a09b8
Thomas Assheton <ta21@sanger.ac.uk>
2025-10-14 16:02:06 +0100

    Add AccountManager class

---
+++ src/main/java/AccountManager.java
@@ -0,0 +1,12 @@
+/**
+ * Performs actions on a BankAccount.
+ * Handles the user input and other tasks that would be less appropriate to be on the BankAccount class.
+ */
+public class AccountManager {
+    // The account to perform actions on
+    final BankAccount account;
+
+    public AccountManager(BankAccount account) {
+        this.account = account;
+    }
+}
