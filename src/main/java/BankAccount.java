90d3e1763f4408e925a41c5820db938fc64a3357
Thomas Assheton <ta21@sanger.ac.uk>
2025-10-14 15:58:38 +0100

    Add balance manipulation methods
    `deposit` adds money to the balance while `withdraw` removes money from the balance.

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,6 +1,7 @@
 public class BankAccount {
     // Constants
     static final double INITIAL_BALANCE = 1_000;
+    static final double SUSPICIOUS_DEPOSIT_AMOUNT = 10_000;
 
     // Fields
     private double balance;
@@ -22,5 +23,35 @@ public class BankAccount {
 
     public User getAccountHolder() {
         return accountHolder;
+    }
+
+    // Balance manipulation methods
+    void deposit(double amount) throws IllegalArgumentException {
+        // Ensure the user does not deposit more money than is suspicious
+        // (for fraud detection purposes)
+        if (amount > SUSPICIOUS_DEPOSIT_AMOUNT) {
+            final String errorMessage = String.format(
+                    "You cannot deposit more than $%.2f using this program\n" +
+                            "Please come into one of our branches in person",
+                    SUSPICIOUS_DEPOSIT_AMOUNT
+            );
+            throw new IllegalArgumentException(errorMessage);
+        }
+
+        // Perform deposit
+        balance += amount;
+    }
+
+    void withdraw(double amount) throws IllegalArgumentException {
+        // Ensure the user does not withdraw more money than they have
+        if (amount > balance) {
+            final String errorMessage = "You cannot withdraw more money than you have\n" +
+                    "Consider enabling overdraft for your account";
+
+            throw new IllegalArgumentException(errorMessage);
+        }
+
+        // Perform withdraw
+        balance -= amount;
     }
 }
