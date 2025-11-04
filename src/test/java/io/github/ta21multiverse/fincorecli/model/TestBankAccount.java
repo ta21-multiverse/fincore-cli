5cb2b6b8d7cf7572ec12979222e1893c292bd3cb
Thomas Assheton <ta21@sanger.ac.uk>
2025-11-04 10:41:09 +0000

    Add unit tests for BankAccount.deposit

---
+++ src/test/java/io/github/ta21multiverse/fincorecli/model/TestBankAccount.java
@@ -0,0 +1,50 @@
+package io.github.ta21multiverse.fincorecli.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestBankAccount {
+    private BankAccount account;
+
+    @BeforeEach
+    void setUpTestBankAccountInstance() {
+        account = new BankAccount(new User("User", "user@example.com", "01632 960 001"));
+    }
+
+    @Test
+    @DisplayName("deposit: Negative amount should be invalid")
+    void testNegativeDeposit() {
+        assertThrows(IllegalArgumentException.class, () -> account.deposit(-20));
+    }
+
+    @Test
+    @DisplayName("deposit: Lower boundary deposit amount should be valid")
+    void testZeroDeposit() {
+        account.deposit(0);
+        assertEquals(BankAccount.INITIAL_BALANCE, account.getBalance());
+    }
+    
+    @Test
+    @DisplayName("deposit: Valid deposit amount")
+    void testValidDeposit() {
+        account.deposit(100);
+        assertEquals(BankAccount.INITIAL_BALANCE + 100, account.getBalance());
+    }
+
+    @Test
+    @DisplayName("deposit: Upper boundary deposit amount should be invalid")
+    void testUpperBoundDeposit() {
+        // After adding `amountToAdd`, the balance should be `SUSPICIOUS_DEPOSIT_AMOUNT`, the exact boundary
+        final double amountToAdd = BankAccount.SUSPICIOUS_DEPOSIT_AMOUNT - BankAccount.INITIAL_BALANCE;
+
+        assertThrows(IllegalArgumentException.class, () -> account.deposit(amountToAdd));
+    }
+
+    @Test
+    @DisplayName("deposit: Invalid deposit amount (too high)")
+    void testInvalidDeposit() {
+        assertThrows(IllegalAccessError.class, () -> account.deposit(BankAccount.SUSPICIOUS_DEPOSIT_AMOUNT));
+    }
+}
